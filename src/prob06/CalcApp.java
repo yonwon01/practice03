71014b2b72325ed1106fb47c8b6d230e83e75a54
kickscar-github <kickscar@gmail.com>
2017-08-02 14:04:52 +0900

    init commit

---
+++ src/prob06/CalcApp.java
@@ -0,0 +1,25 @@
+package prob06;
+
+import java.util.Scanner;
+
+public class CalcApp {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		while( true ) {
+			
+			/*  코드를 완성 합니다 */
+			System.out.print( ">> " );
+			String expression = scanner.nextLine();
+			
+			if( "quit".equals( expression ) ) {
+				break;
+			}
+		}
+		
+		scanner.close();
+
+	}
+
+}
